7e101fdae725401d8cd18b7d17cb1b86574a1748
Arvina Jane <126264428+arvinacagayat@users.noreply.github.com>
2023-05-06 10:59:41 +0800

    Add files via upload

---
+++ TaskPerf6.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import static java.nio.file.StandardOpenOption.*;
+
+public class TaskPerf6 {
